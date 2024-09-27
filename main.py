a24d811fadc824e04b239403f3f090995e135bde
Fernando Peres Fernandes <fernando.peres.tech@gmail.com>
2024-09-26 21:02:09 -0300

    implementação da função que coleta valores de abertura e fechamento da B3 dos últimos 4 meses

--- main.py
+++ main.py
@@ -5,7 +5,10 @@ import subprocess
 import pandas as pd
 import yfinance as yf
 import investpy as inv
+from datetime import datetime
+from dateutil.relativedelta import relativedelta
 
+logging.basicConfig(filename='logs/logsSistem.log', encoding='utf-8', level=logging.DEBUG)
 
 ''' função para criar estrutura de pastas, para receber dados e registrar os Logs do sistema '''
 def folder_create(folder_list : list):
@@ -42,7 +45,7 @@ def get_ativos():
         
         for index, item in enumerate(lista_ativos_investpy, start=1):
             sheet.cell(row=index, column=1, value=item)
-        workbook.save('data/tickers_investpy.xlsx')
+        workbook.save('data/investpy_tickers_b3.xlsx')
 
     except Exception as e:
         logging.warning(f'main.get_ativos(): {str(e)}')
@@ -50,29 +53,72 @@ def get_ativos():
 ''' Buscando ativos da B3 e gravando no arquivo -> data\tickers_investpy.xlsx'''
 get_ativos()
 
-''' BLOCO DE CÓDIGO PARA OBTENÇÃO DOS DADOS DOS ATIVOS '''
-
+''' Definição da função que coletará os dados dos papéis da B3 '''
 def obter_dados(ticker, start_date, end_date):
     try:
-        data = yf.download(ticker, start=start_date, end=end_date)
-
-        data['Ticker'] = ticker
-        data['Data'] = data.index
+        # Faz o download dos dados históricos do ticker
+        dados_ticker = yf.download(ticker, start=start_date, end=end_date)
         
-        data = data[['Ticker', 'Data', 'Open', 'High', 'Low', 'Close', 'Adj Close', 'Volume']]
+        if dados_ticker.empty:
+            logging.warning(f'Dados não encontrados para {ticker}.')
+            return pd.DataFrame()  # Retorna DataFrame vazio se não houver dados
 
-        return data
-    
+        # Adiciona a coluna do ticker e de data
+        dados_ticker['Ticker'] = ticker
+        dados_ticker['Data'] = dados_ticker.index
+
+        # Reorganiza as colunas na ordem desejada
+        dados_ticker = dados_ticker[['Ticker', 'Data', 'Open', 'High', 'Low', 'Close', 'Adj Close', 'Volume']]
+        
+        return dados_ticker
     except Exception as e:
-        logging.warning(f'main.obter_dados(): {str(e)}')
+        logging.warning(f'Erro ao obter dados para {ticker}: {str(e)}')
+        return pd.DataFrame()  # Retorna DataFrame vazio em caso de erro
 
-def verificar_e_criar_arquivo(path):
+def verificar_e_criar_arquivo(caminho):
+    # Verifica se o arquivo existe, caso contrário, irá criar o arquivo
     try:
-        if not os.path.exists(path):
-            dados_vazios = pd.DataFrame()
-            dados_vazios.to_csv(path, index=False)
+        open(caminho, 'a').close()  # Cria o arquivo se não existir
     except Exception as e:
-        logging.warning(f'main.verificar_e_criar_arquivo(): {str(e)}')
+        logging.warning(f'Erro ao verificar/criar arquivo: {str(e)}')
 
+# Função principal para buscar os dados
+def iniciar_busca_dados():
+    try:
+        caminho_tickers = 'data/tickers_investpy.xlsx'
 
+        # Carrega os tickers do arquivo Excel
+        tickers_df = pd.read_excel(caminho_tickers, usecols=[0], skiprows=1, header=None, names=['Ticker'])
 
+        tickers = tickers_df['Ticker'].tolist()
+
+        end_date = datetime.now()
+        start_date = end_date - relativedelta(months=4)
+
+        formated_end_date = end_date.strftime("%Y-%m-%d")
+        formated_start_date = start_date.strftime("%Y-%m-%d")
+
+        caminho_dados = 'data/yfinance_tickers_results.csv'
+
+        # Verifica e cria arquivo para armazenar os dados
+        verificar_e_criar_arquivo(caminho_dados)
+
+        dados_totais = pd.DataFrame()
+
+        for ticker in tickers:
+            ticker = ticker + '.SA'
+            print(f"Buscando dados para: {ticker}")
+            dados_ticker = obter_dados(ticker, formated_start_date, formated_end_date)
+
+            if not dados_ticker.empty:
+                dados_totais = pd.concat([dados_totais, dados_ticker])
+
+        if not dados_totais.empty:
+            # Salva todos os dados no arquivo CSV
+            dados_totais.to_csv(caminho_dados, index=False)
+
+    except Exception as e:
+        logging.warning(f'Erro ao iniciar busca de dados: {str(e)}')
+
+# Executa a função principal
+iniciar_busca_dados()
